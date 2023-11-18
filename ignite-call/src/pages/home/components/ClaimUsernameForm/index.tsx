80ff5924f73055ad8c618ef932604cb4c29ccd52
gabriel <gabrielcomputacao@outlook.com>
2023-11-18 12:42:19 -0300

    feat: creating  forms submiting  using react hook form and zod

--- ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -4,6 +4,7 @@ import { ArrowRight } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
 
 const claimUsernameFormSchema = z.object({
   username: z
@@ -19,13 +20,17 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   });
 
+  const router = useRouter();
+
   async function handleClaimUsername(data: ClaimUsernameFormData) {
-    console.log(data.username);
+    const { username } = data;
+
+    router.push(`/register?username=${username}`);
   }
 
   return (
@@ -38,7 +43,7 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuario"
           {...register("username")}
         ></TextInput>
-        <Button type="submit" size="sm">
+        <Button type="submit" size="sm" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
--- ignite-call/src/pages/register/index.page.tsx
+++ ignite-call/src/pages/register/index.page.tsx
@@ -1,8 +1,49 @@
 import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
-import { Container, Form, Header } from "./styles";
+import { Container, Form, FormError, Header } from "./styles";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Tamanho inválido" })
+    .regex(/^([a-z\\-]+)$/i, { message: "Não pode ter caracter especial" })
+    .transform((username) => username.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: "Tamanho inválido" })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "o nome não pode ter caracter especial",
+    })
+    .transform((username) => username.toLowerCase()),
+});
+
+type RegisterFormData = z.infer<typeof registerFormSchema>;
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue("username", String(router.query.username));
+    }
+  }, [router.query?.username, setValue]);
+
+  async function handleRegister(data: RegisterFormData) {}
+
   return (
     <Container>
       <Header>
@@ -11,21 +52,34 @@ export default function Register() {
 
         <MultiStep size={4} currentStep={1} />
 
-        <Form as="form">
+        <Form as="form" onSubmit={handleSubmit(handleRegister)}>
           <label>
             <Text size="sm">Nome do usuário</Text>
             <TextInput
               crossOrigin=""
               prefix="ingite.com/"
               placeholder="seu-usuario"
+              {...register("username")}
             />
+
+            {errors.username && (
+              <FormError size="sm">{errors.username.message}</FormError>
+            )}
           </label>
           <label>
             <Text size="sm">Nome Completo</Text>
-            <TextInput crossOrigin="" placeholder="seu nome" />
+            <TextInput
+              crossOrigin=""
+              placeholder="seu nome"
+              {...register("name")}
+            />
+
+            {errors.name && (
+              <FormError size="sm">{errors.name.message}</FormError>
+            )}
           </label>
 
-          <Button>
+          <Button disabled={isSubmitting}>
             Próximo Passo
             <ArrowRight />
           </Button>
