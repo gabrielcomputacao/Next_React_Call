555fe3e17d8e3a2eacb58041a323da4bbf73c428
gabriel <gabrielcomputacao@outlook.com>
2023-11-20 07:03:01 -0300

    feat: configuring cookies and handleRequest using axios

--- ignite-call/src/pages/register/index.page.tsx
+++ ignite-call/src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { api } from "@/lib/axios";
+import { AxiosError } from "axios";
 
 const registerFormSchema = z.object({
   username: z
@@ -13,13 +15,7 @@ const registerFormSchema = z.object({
     .min(3, { message: "Tamanho inválido" })
     .regex(/^([a-z\\-]+)$/i, { message: "Não pode ter caracter especial" })
     .transform((username) => username.toLowerCase()),
-  name: z
-    .string()
-    .min(3, { message: "Tamanho inválido" })
-    .regex(/^([a-z\\-]+)$/i, {
-      message: "o nome não pode ter caracter especial",
-    })
-    .transform((username) => username.toLowerCase()),
+  name: z.string().min(3, { message: "Tamanho inválido" }),
 });
 
 type RegisterFormData = z.infer<typeof registerFormSchema>;
@@ -42,7 +38,22 @@ export default function Register() {
     }
   }, [router.query?.username, setValue]);
 
-  async function handleRegister(data: RegisterFormData) {}
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post("/users", {
+        name: data.name,
+        username: data.username,
+      });
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message);
+
+        return;
+      }
+
+      console.log(err);
+    }
+  }
 
   return (
     <Container>
