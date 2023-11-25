095966a2afd5f51ac13e2f91b46cca8fcae12822
gabriel <gabrielcomputacao@outlook.com>
2023-11-25 14:59:08 -0300

    feat: creating calendar step

--- ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
 import { Calendar } from "@/components/Calendar";
-import { ContainerSchedule } from "./styles";
+import {
+  ContainerSchedule,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
 
 export function CalendarStep() {
+  const isDateSelected = true;
+
   return (
-    <ContainerSchedule>
+    <ContainerSchedule isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Sabado <span>25 de novembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </ContainerSchedule>
   );
 }
