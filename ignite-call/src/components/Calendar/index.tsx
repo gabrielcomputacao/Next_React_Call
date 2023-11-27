3398b88bde9ad8966932586f561d8546eceb2b10
gabriel <gabrielcomputacao@outlook.com>
2023-11-26 22:53:05 -0300

    fix: correcting ordering of days

--- ignite-call/src/components/Calendar/index.tsx
+++ ignite-call/src/components/Calendar/index.tsx
@@ -20,7 +20,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[];
 
-export function Calendar() {
+interface CalendarProps {
+  selectedDate?: Date | null;
+  onDateSelected: (date: Date) => void;
+}
+
+export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set("date", 1);
   });
@@ -74,12 +79,15 @@ export function Calendar() {
       return lastDayInCurrentMonth.add(i + 1, "day");
     });
 
+    console.log(previousMonthFillArray);
+    console.log(daysInMonthArray);
+
     const calendarDays = [
-      ...daysInMonthArray.map((date) => {
-        return { date, disabled: false };
-      }),
       ...previousMonthFillArray.map((date) => {
         return { date, disabled: true };
+      }),
+      ...daysInMonthArray.map((date) => {
+        return { date, disabled: date.endOf("day").isBefore(new Date()) };
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true };
@@ -141,7 +149,10 @@ export function Calendar() {
                 {days.map(({ date, disabled }) => {
                   return (
                     <td key={date.toString()}>
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay
+                        onClick={() => onDateSelected(date.toDate())}
+                        disabled={disabled}
+                      >
                         {date.get("date")}
                       </CalendarDay>
                     </td>
