6d5304776bad54fd35decd5736198ae84dc1cfba
gabriel <gabrielcomputacao@outlook.com>
2023-11-30 13:25:46 -0300

    refactor:  improving SEO

--- ignite-call/src/pages/home/index.tsx
+++ ignite-call/src/pages/home/index.tsx
@@ -3,28 +3,33 @@ import { Container, Hero, Preview } from "./styles";
 import Image from "next/image";
 import previewImage from "../../assets/app_preview.png";
 import { ClaimUsernameForm } from "./components/ClaimUsernameForm";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento Descomplicado</Heading>
-        <Text size="lg">
-          Conecte seu calendario e permita que as pessoas marquem agendamentos
-          no seu tempo.
-        </Text>
+    <>
+      <NextSeo title="Agenda Suas reuniões" description="Agora é a hora." />
 
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          alt="Calendário imitando a aplicação"
-          height={400}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento Descomplicado</Heading>
+          <Text size="lg">
+            Conecte seu calendario e permita que as pessoas marquem agendamentos
+            no seu tempo.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            alt="Calendário imitando a aplicação"
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   );
 }
